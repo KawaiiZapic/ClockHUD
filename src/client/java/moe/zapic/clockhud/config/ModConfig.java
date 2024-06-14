4c3f9b7a4b050171da65ab046bc5d4743fc1bdd0
KawaiiZapic <kawaiizapic@zapic.moe>
2024-06-15 01:35:00 +0800

    feat: merge fix from 1.21 version

--- src/client/java/moe/zapic/clockhud/config/ModConfig.java
+++ src/client/java/moe/zapic/clockhud/config/ModConfig.java
@@ -1,14 +1,11 @@
 package moe.zapic.clockhud.config;
 
-import com.terraformersmc.modmenu.api.ConfigScreenFactory;
-import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
 import me.shedaniel.autoconfig.annotation.Config;
 
 @Config(name = "clock-hud")
-public class ModConfig implements ConfigData, ModMenuApi {
+public class ModConfig implements ConfigData {
     public boolean isShow = true;
     public boolean showDayCount = true;
     public int ScreenX = 4;
@@ -22,8 +19,4 @@ public class ModConfig implements ConfigData, ModMenuApi {
     @ConfigEntry.BoundedDiscrete(min = 0, max = 255)
     public int Opacity = 255;
 
-    @Override
-    public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> AutoConfig.getConfigScreen(ModConfig.class, parent).get();
-    }
 }
