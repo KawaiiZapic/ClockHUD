beed98ef665beb45cea1e7e3fb02d617d64f945d
Zapic <kawaiizapic@zapic.moe>
2025-09-14 11:46:24 +0800

    improve: optimize clock render logic

--- src/client/java/moe/zapic/clockhud/render/ClockRender.java
+++ src/client/java/moe/zapic/clockhud/render/ClockRender.java
@@ -8,16 +8,18 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.ColorHelper;
 
 public class ClockRender {
     public static void render(DrawContext context, RenderTickCounter rtc) {
         var config = Main.config;
         if (!config.isShow) { return; }
 
+        var color = ColorHelper.fromFloats(config.Opacity / 255.0f, 1.0f,1.0f,1.0f);
+
         RenderSystem.enableBlend();
         RenderSystem.defaultBlendFunc();
         RenderSystem.disableDepthTest();
-        RenderSystem.setShaderColor(1.0f,1.0f,1.0f,config.Opacity / 255.0f);
         var matrices = context.getMatrices();
         matrices.push();
 
@@ -26,16 +28,15 @@ public class ClockRender {
 
         var RealX = config.ScreenX + (Textures.ICON_S - Textures.DOT_S) / 2;
         var RealY = config.ScreenY + (Textures.ICON_S - Textures.BAR_H) / 2 - 1;
-        drawTexture(context, Textures.BAR, RealX, RealY, Textures.BAR_W, Textures.BAR_H);
+        drawTexture(context, Textures.BAR, RealX, RealY, Textures.BAR_W, Textures.BAR_H, color);
 
         var IconX = config.ScreenX + (int) (Utils.getScaleTime() * (Textures.BAR_W - Textures.DOT_S));
-        drawTexture(context, Utils.isDay() ? Textures.SUN : Textures.MOON, IconX, config.ScreenY, Textures.ICON_S, Textures.ICON_S);
+        drawTexture(context, Utils.isDay() ? Textures.SUN : Textures.MOON, IconX, config.ScreenY, Textures.ICON_S, Textures.ICON_S, color);
 
         matrices.pop();
-        RenderSystem.setShaderColor(1.0f,1.0f,1.0f,1.0f);
     }
 
-    private static void drawTexture(DrawContext context, Identifier texture, int x, int y, int w, int h) {
-        context.drawTexture(RenderLayer::getGuiTextured, texture, x, y, 0f, 0f, w, h, w, h);
+    private static void drawTexture(DrawContext context, Identifier texture, int x, int y, int w, int h, int color) {
+        context.drawTexture(RenderLayer::getGuiTextured, texture, x, y, 0f, 0f, w, h, w, h, color);
     }
 }
--- src/client/java/moe/zapic/clockhud/render/DayCountRender.java
+++ src/client/java/moe/zapic/clockhud/render/DayCountRender.java
@@ -16,7 +16,6 @@ public class DayCountRender {
 
     public static void render(DrawContext context, RenderTickCounter rtc) {
         if(!Main.config.showDayCount) return;
-        checkIsNewDay();
         if (!isTextRendering) return;
         var mc = MinecraftClient.getInstance();
         var matrices = context.getMatrices();
@@ -40,25 +39,20 @@ public class DayCountRender {
             TextOpacity = (int) ((0xff * (renderTime / 20)) + 4);
         } else if (renderTime >= 80) {
             TextOpacity = (int) ((0xff * ((Duration - renderTime) / 20)) + 4);
-        }  else if (TextOpacity != 0xff) {
+        } else if (TextOpacity != 0xff) {
             TextOpacity = 0xff;
         }
     }
 
-    public static long getDayCount() {
+    public static void checkIsNewDay() {
         var world = MinecraftClient.getInstance().world;
         assert world != null;
-        return world.getTimeOfDay() / Utils.DAY_TICKS;
-    }
-
-    public static void checkIsNewDay() {
-        var day = getDayCount();
+        var day = world.getTimeOfDay() / Utils.DAY_TICKS;
         if (currentDay == -1) {
             currentDay = day;
         }
         if (day == currentDay) return;
-
-        isTextRendering = day > currentDay;
+        isTextRendering = true;
         currentDay = day;
     }
 }
