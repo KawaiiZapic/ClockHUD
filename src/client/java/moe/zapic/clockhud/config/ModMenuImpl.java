0bdd70b916962a20dd84720daf047fd190ae624f
Zapic <kawaiizapic@zapic.moe>
2026-01-03 13:53:58 +0800

    feat: support 1.21.11

--- src/client/java/moe/zapic/clockhud/config/ModMenuImpl.java
+++ src/client/java/moe/zapic/clockhud/config/ModMenuImpl.java
@@ -2,11 +2,11 @@ package moe.zapic.clockhud.config;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.AutoConfigClient;
 
 public class ModMenuImpl implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> AutoConfig.getConfigScreen(ModConfig.class, parent).get();
+        return parent -> AutoConfigClient.getConfigScreen(ModConfig.class, parent).get();
     }
 }
